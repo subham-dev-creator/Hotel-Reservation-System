693e6146fd4da29d548f0ed67cd4f563c0d52d2e
subhamstark <subham@sap.com>
2020-10-11 11:32:54 +0530

    inital commit

---
+++ src/com/training/hotelreservationsystem/Hotel.java
@@ -0,0 +1,7 @@
+package com.training.hotelreservationsystem;
+
+public class Hotel {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Hotel Reservation System");
+    }
+}
