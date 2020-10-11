5213fc5e0c99ff3fe43a5a00dafaccdd6c0c0915
subhamstark <subham@sap.com>
2020-10-11 17:05:58 +0530

    UC1 Done

--- src/com/training/hotelreservationsystem/Hotel.java
+++ src/com/training/hotelreservationsystem/Hotel.java
@@ -1,7 +1,39 @@
 package com.training.hotelreservationsystem;
 
+import java.util.Scanner;
+
 public class Hotel {
+    String HotelName;
+    double rate;
+
+    public Hotel(String hotelName, double rate) {
+        HotelName = hotelName;
+        this.rate = rate;
+    }
+
+    public String getHotelName() {
+        return HotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        HotelName = hotelName;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Hotel Reservation System");
+        Scanner SCANNEROBJ = new Scanner(System.in);
+        System.out.println("Enter the Hotel name and rates");
+        String name =SCANNEROBJ.next();
+        double rate = SCANNEROBJ.nextInt();
+
+        Hotel hotel1 = new Hotel(name,rate);
+        System.out.println("Hotel " + hotel1.getHotelName() + " has rate " + hotel1.getRate());
     }
 }
